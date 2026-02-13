59dd9bc3e60b875cf8d73c142fda2fee416c67b9
Tomás Ezequiel Martínez <tomas.emart@gmail.com>
2026-02-12 23:00:10 -0300

    creación del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TPI_GESTION_HOGAR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
