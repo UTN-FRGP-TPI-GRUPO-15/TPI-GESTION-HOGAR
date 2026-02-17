154bdcb7bd2aeeefa29d67558969e44c9901fde6
Tomás Ezequiel Martínez <tomas.emart@gmail.com>
2026-02-17 14:34:12 -0300

    feat: crear db y/o aplicar migraciones automaticamente al correr el proyecto

--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 //Cofiguramos cadena de concexión a la base de datos
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//Crear base si no existe y/o aplicar migraciones pendientes
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+
+    //Acá se puede hacer seed de registros necesarios por defecto (por ejemplo roles de usuario, usuarios de prueba, mujeres de prueba, etc.)
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
