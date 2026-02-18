95489de54c297e0b37e034d68b0f25476d20d406
Tomás Ezequiel Martínez <tomas.emart@gmail.com>
2026-02-18 18:41:06 -0300

    feat: agregar pantalla de login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,19 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using TPI_GESTION_HOGAR.Datos;
 using TPI_GESTION_HOGAR.Models;
 
 namespace TPI_GESTION_HOGAR.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +21,28 @@ namespace TPI_GESTION_HOGAR.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string usuario, string password)
+        {
+            var user = _context.Usuarios.FirstOrDefault(u => u.NombreUsuario == usuario && u.Clave == password);
+
+            if (user == null)
+            {
+                ViewBag.Message = "Credenciales incorrectas";
+                ViewBag.IsError = true;
+                return View();
+            }
+
+            ViewBag.Message = "Sesión iniciada correctamente";
+            ViewBag.IsError = false;
             return View();
         }
 
