520ede23b0fb2520d89286edf98c04bf66c6de95
Tomás Ezequiel Martínez <tomas.emart@gmail.com>
2026-02-24 22:53:34 -0300

    feat: actualizar modelos de Personal, Rol y Usuario

--- Models/Personal.cs
+++ Models/Personal.cs
@@ -2,20 +2,20 @@
 {
     public class Personal
     {
-        public int ID { get; set; }
-        public required int Legajo { get; set; }
+        public int Id { get; set; }
+        public int Legajo { get; set; }
         public required string Apellido { get; set; }
         public required string Nombre { get; set; }
-        public required int DNI { get; set; }
+        public required string DNI { get; set; }
         public required string Nacionalidad { get; set; }
-        public required DateOnly FechaNac { get; set; }
+        public DateOnly FechaNac { get; set; }
         public string? Telefono { get; set; }
         public string? Domicilio { get; set; }
         public string? Localidad { get; set; }
-        public bool estado { get; set; }
+        public bool Activo { get; set; }
 
-        public Usuario? Usuario { get; set; }
+        public Usuario Usuario { get; set; } = null!;
 
-        public List<Turno>? Turno { get; set; }
+        public ICollection<Turno> Turnos { get; set; } = [];
     }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -4,14 +4,15 @@
     {
         public int Id { get; set; }
         public required string NombreUsuario { get; set; }
+        public required string Email { get; set; }
         public required string Clave { get; set; }
 
         //FK
         public int PersonalId { get; set; }
-        public required int RolId { get; set; }
+        public int RolId { get; set; }
 
         //Relaciones
-        public Personal? Personal { get; set; }
-        public Rol? Rol { get; set; }
+        public Personal Personal { get; set; } = null!;
+        public Rol Rol { get; set; } = null!;
     }
 }
