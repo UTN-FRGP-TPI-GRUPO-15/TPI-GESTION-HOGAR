5700be68bd03cb3f01368e042f54aef9b3a1c71e
Elian Varela <elian_varela@outlook.com.ar>
2026-02-26 19:55:47 -0300

    Vista Historial de Turnos Orden por fecha y nombre Filtrado por nombre

--- Controllers/TurnosController.cs
+++ Controllers/TurnosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using TPI_GESTION_HOGAR.Datos;
 using TPI_GESTION_HOGAR.Models;
@@ -16,13 +17,74 @@ namespace TPI_GESTION_HOGAR.Controllers
         }
 
         //GET: Turnos
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString, string sortOrder)
         {
+            ViewData["CurrentFilter"] = searchString; 
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["NameSortParm"] = sortOrder == "Name" ? "name_desc" : "Name";
+
             var turnos = await _context.Turnos
                 .Include(t => t.TipoTurno)
                 .Include(t => t.Personal)
                 .ToListAsync();
 
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                turnos = turnos.Where(t => t.Personal.Apellido.Contains(searchString) || t.Personal.Nombre.Contains(searchString)).ToList();
+            }
+
+            switch (sortOrder)
+            {
+                case "date_desc":
+                    turnos = turnos.OrderByDescending(t => t.Fecha).ToList();
+                    break;
+                case "Name":
+                    turnos = turnos.OrderBy(t => t.Personal.Apellido).ToList();
+                    break;
+                case "name_desc":
+                    turnos = turnos.OrderByDescending(t => t.Personal.Apellido).ToList();
+                    break;
+                default:
+                    turnos = turnos.OrderBy(t => t.Fecha).ToList();
+                    break;
+            }
+
+            return View(turnos);
+        }
+
+        //GET: Turnos/History
+        public async Task<IActionResult> History(string searchString, string sortOrder)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["NameSortParm"] = sortOrder == "Name" ? "name_desc" : "Name";
+
+            var turnos = await _context.Turnos
+                .Include(t => t.TipoTurno)
+                .Include(t => t.Personal)
+                .ToListAsync();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                turnos = turnos.Where(t => t.Personal.Apellido.Contains(searchString) || t.Personal.Nombre.Contains(searchString)).ToList();
+            }
+
+            switch (sortOrder)
+            {
+                case "date_desc":
+                    turnos = turnos.OrderByDescending(t => t.Fecha).ToList();
+                    break;
+                case "Name":
+                    turnos = turnos.OrderBy(t => t.Personal.Apellido).ToList();
+                    break;
+                case "name_desc":
+                    turnos = turnos.OrderByDescending(t => t.Personal.Apellido).ToList();
+                    break;
+                default:
+                    turnos = turnos.OrderBy(t => t.Fecha).ToList();
+                    break;
+            }
+                       
             return View(turnos);
         }
 
@@ -36,12 +98,125 @@ namespace TPI_GESTION_HOGAR.Controllers
             var personal = _context.Personal.Select(e => new
             {
                 ID = e.ID,
-                Name = e.Nombre + " " + e.Apellido
+                Name = e.Apellido + ", " + e.Nombre
             });
 
-            ViewBag.Personal = new SelectList(personal, "ID", "Name");
+            var listaAlfabetica = personal.OrderBy(item => item.Name).ToList();
+
+            ViewBag.Personal = new SelectList(listaAlfabetica, "ID", "Name");
 
             return View();
+        }
+
+        //POST: Turnos/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Turno turno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(turno);
+        }
+
+        //GET: Turnos/Edit
+
+        public async Task<IActionResult> Edit(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos
+                .Include(t => t.TipoTurno)
+                .Include(t => t.Personal)
+                .FirstOrDefaultAsync(t => t.ID == Id);
+
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            var tipoTurnos = _context.TipoTurnos.ToList();
+            ViewBag.TipoTurnos = new SelectList(tipoTurnos, "Id", "Descripcion");
+
+            var personal = _context.Personal.Select(e => new
+            {
+                ID = e.ID,
+                Name = e.Apellido + ", " + e.Nombre
+            });
+
+            var listaAlfabetica = personal.OrderBy(item => item.Name).ToList();
+
+            ViewBag.Personal = new SelectList(listaAlfabetica, "ID", "Name");
+
+            return View(turno);
+        }
+
+        //POST: Turnos/Edit
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int Id, Turno turno)
+        {
+            if(Id != turno.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(turno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(turno);
+        }
+
+        //GET: Turnos/Delete
+
+        public async Task<IActionResult> Delete(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos
+                .Include(t => t.TipoTurno)
+                .Include(t => t.Personal)
+                .FirstOrDefaultAsync(t => t.ID == Id);
+
+            if (turno == null)
+            {
+                return NotFound();
+            }
+                      
+            return View(turno);
+        }
+
+        //POST: Turnos/Delete
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int Id)
+        {
+            var turno = await _context.Turnos.FindAsync(Id);
+
+            if (turno != null)
+            {
+                _context.Turnos.Remove(turno);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Models/Turno.cs
+++ Models/Turno.cs
@@ -1,12 +1,18 @@
-﻿namespace TPI_GESTION_HOGAR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPI_GESTION_HOGAR.Models
 {
     public class Turno
     {
         public required int ID { get; set; }
-        public required DateOnly Fecha { get; set; }     
-        
+
+        [Required(ErrorMessage = "Debe ingresar la fecha")]
+        public required DateOnly Fecha { get; set; }
+
         //FK
+        [Required(ErrorMessage = "Debe ingresar el horario")]
         public required int TipoTurnoId { get; set; }
+        [Required(ErrorMessage = "Debe ingresar el personal")]
         public required int  PersonalId { get; set; }
 
         //Relaciones
