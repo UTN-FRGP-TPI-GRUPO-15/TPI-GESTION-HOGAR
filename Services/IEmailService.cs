e658e10068274266f72e234395ba904c282e5b0c
Tomás Ezequiel Martínez <tomas.emart@gmail.com>
2026-03-06 21:42:29 -0300

    feat: implementar servicio de emails

---
+++ Services/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace TPI_GESTION_HOGAR.Services
+{
+    public interface IEmailService
+    {
+        Task SendPasswordResetAsync(string toEmail, string resetLink);
+        Task SendNotificationAsync(string toEmail, string subject, string message);
+    }
+}
